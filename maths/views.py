31962155c126c2cd287b0219c939247cc5dbc90c
Magda Ratajczak <magda.ratajczak1986@gmail.com>
2022-10-20 20:15:25 +0200

    dzialajacy kalkulator

--- maths/views.py
+++ maths/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+
+def math(request):
+    t = loader.get_template("maths/main.html")
+    return HttpResponse(t.render())
+
+
+def add(request, a, b):
+    a, b = int(a), int(b)
+    wynik = a + b
+    t = loader.get_template("maths/operation.html")
+    c = {"a": a, "b": b, "operacja": "+", "wynik": wynik, "title": "dodawanie"}
+    return HttpResponse(t.render(c))
+
+
+def sub(request, a, b):
+    a, b = int(a), int(b)
+    wynik = a - b
+    t = loader.get_template("maths/operation.html")
+    c = {"a": a, "b": b, "operacja": "-", "wynik": wynik, "title": "odejmowanie"}
+    return HttpResponse(t.render(c))
+
+
+def mul(request, a, b):
+    a, b = int(a), int(b)
+    wynik = a * b
+    t = loader.get_template("maths/operation.html")
+    c = {"a": a, "b": b, "operacja": "*", "wynik": wynik, "title": "mnożenie"}
+    return HttpResponse(t.render(c))
+
+
+def div(request, a, b):
+    a, b = int(a), int(b)
+    if b == 0:
+        return HttpResponse("Nie dziel przez 0")
+    wynik = a / b
+    t = loader.get_template("maths/operation.html")
+    c = {"a": a, "b": b, "operacja": "/", "wynik": wynik, "title": "dzielenie"}
+    return HttpResponse(t.render(c))
+
+
+
+#def div(request, a, b):
+ #  a, b = int(a), int(b)
+  # if b == 0:
+   #    return HttpResponse("Nie dziel przez 0")
+   #return HttpResponse(a / b)
