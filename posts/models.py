9526ce9ddda66f69210ad37c464822af07a3675e
Magda Ratajczak <magda.ratajczak1986@gmail.com>
2022-10-22 21:25:36 +0200

    posts3

--- posts/models.py
+++ posts/models.py
@@ -6,12 +6,16 @@ class Post(models.Model):
     #id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=20)
     content = models.TextField()
-    created = models.DateTimeFiled(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     author = models.EmailField(max_length=50)
+    def __str__(self):
+        return f"id:{self.id}, title:{self.title}, author:{self.author}"
 
 class Author(models.Model):
-    nick = models.CharField(max_length=10)
-    email = models.EmailField(max_length=50)
+    nick = models.CharField(max_length=10, unique=True)
+    email = models.EmailField(max_length=50, unique=True)
+    def __str__(self):
+        return f"nick:{self.nick}, email:{self.email}"
 
 # Create your models here.
