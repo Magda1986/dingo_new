4c657666d2457013eb45ff0cfc93399bb6da4c02
Magda Ratajczak <magda.ratajczak1986@gmail.com>
2022-10-25 10:32:15 +0200

    posts-v5

--- maths/admin.py
+++ maths/admin.py
@@ -1,3 +1,15 @@
+# maths/admin.py
 from django.contrib import admin
-
+from maths.models import Math, Result
 # Register your models here.
+
+class MathAdmin(admin.ModelAdmin):
+    list_display = ["id", "operation", "a", "b", "created", "result"]    #to lista kolumn widoczna na liście
+    list_filter = ["operation"]    #dodaje boczny panel z filtrami,
+    search_fields = ["a", "b"]  #dodaje pole wyszukiwania. Wyszukiwanie odbywa się we wskazanych kolumnach
+
+admin.site.register(Math, MathAdmin)
+
+@admin.register(Result)
+class ResultAdmin(admin.ModelAdmin):
+    list_display = ['id', 'value', 'error']
--- maths/views.py
+++ maths/views.py
@@ -10,25 +10,25 @@ def math(request):
 
 def add(request, a, b):
     a, b = int(a), int(b)
-    wynik = a + b
+    result = a + b
     t = loader.get_template("maths/operation.html")
-    c = {"a": a, "b": b, "operacja": "+", "wynik": wynik, "title": "dodawanie"}
+    c = {"a": a, "b": b, "operacja": "+", "wynik": result, "title": "dodawanie"}
     return HttpResponse(t.render(c))
 
 
 def sub(request, a, b):
     a, b = int(a), int(b)
-    wynik = a - b
+    result = a - b
     t = loader.get_template("maths/operation.html")
-    c = {"a": a, "b": b, "operacja": "-", "wynik": wynik, "title": "odejmowanie"}
+    c = {"a": a, "b": b, "operacja": "-", "wynik": result, "title": "odejmowanie"}
     return HttpResponse(t.render(c))
 
 
 def mul(request, a, b):
     a, b = int(a), int(b)
-    wynik = a * b
+    result = a * b
     t = loader.get_template("maths/operation.html")
-    c = {"a": a, "b": b, "operacja": "*", "wynik": wynik, "title": "mnożenie"}
+    c = {"a": a, "b": b, "operacja": "*", "wynik": result, "title": "mnożenie"}
     return HttpResponse(t.render(c))
 
 
@@ -36,10 +36,26 @@ def div(request, a, b):
     a, b = int(a), int(b)
     if b == 0:
         return HttpResponse("Nie dziel przez 0")
-    wynik = a / b
+    result = a / b
     t = loader.get_template("maths/operation.html")
-    c = {"a": a, "b": b, "operacja": "/", "wynik": wynik, "title": "dzielenie"}
+    c = {"a": a, "b": b, "operacja": "/", "wynik": result, "title": "dzielenie"}
     return HttpResponse(t.render(c))
+
+def maths_list(request):
+    maths = Math.objects.all()
+    return render(
+        request=request,
+        template_name="maths/list.html",
+        context={"maths": maths}
+    )
+
+def math_details(request, id):
+    math = Math.objects.get(id=id)
+    return render(
+        request=request,
+        template_name="maths/details.html",
+        context={"math": math}
+    )
 
 
 
